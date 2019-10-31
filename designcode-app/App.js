1ea003da125d20774f9b877582661db34f28aaaa
Dexter Wang <shuenwang@gmail.com>
2019-10-30 22:02:08 -0500

    Up to beginning of lesson 2

--- designcode-app/App.js
+++ designcode-app/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <Title>Welcome back,</Title>
+      <Name>Meng</Name>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  background: #f0f3f5;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.View``;
+const Name = styled.View``;
