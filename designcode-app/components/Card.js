2a4404d2d6a5f73b69d0fa25ca47bd2e3b9452c1
Dexter Wang <shuenwang@gmail.com>
2019-11-01 23:53:07 -0500

    Update Card.js

--- designcode-app/components/Card.js
+++ designcode-app/components/Card.js
@@ -7,6 +7,13 @@ const Card = props => (
       <Image source={require("../assets/background2.jpg")} />
       <Title>Styled Components</Title>
     </Cover>
+    <Content>
+      <Logo source={require("../assets/logo-react.png")} />
+      <Wrapper>
+        <Caption>React Native</Caption>
+        <Subtitle>5 of 12 sections</Subtitle>
+      </Wrapper>
+    </Content>
   </Container>
 );
 
@@ -27,11 +34,51 @@ const Container = styled.View`
 const Cover = styled.View`
   width: 100%;
   height: 200px;
+  border-top-left-radius: 14px;
+  overflow: hidden;
 `;
 
 const Image = styled.Image`
   width: 100%;
   height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
-const Title = styled.Text``;
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 20px;
+  width: 170px;
+`;
+
+const Content = styled.View`
+  padding-left: 20px;
+  height: 80px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Wrapper = styled.View`
+  margin-left: 20px;
+`;
+
+const Logo = styled.Image`
+  width: 44px;
+  height: 44px;
+`;
+const Caption = styled.Text`
+  color: #3c4560;
+  font-size: 20px;
+  font-weight: 600;
+`;
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  text-transform: uppercase;
+  margin-top: 4px;
+`;
